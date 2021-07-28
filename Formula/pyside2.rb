93aaeec1589502b6471de08a44c1d7517cad2dfc
chris <chris.r.jones.1983@gmail.com>
2021-07-28 10:48:10 -0500

    remove some white space in the `pyside2` formula file

--- Formula/pyside2.rb
+++ Formula/pyside2.rb
@@ -16,11 +16,9 @@ class Pyside2 < Formula
   option "without-docs", "Skip building documentation"
 
   depends_on "./python3.9" => :build
-
   depends_on "cmake" => :build
   depends_on "sphinx-doc" => :build if build.with? "docs"
   depends_on "./qt5152"
-
   depends_on "./shiboken2"
 
   def install
