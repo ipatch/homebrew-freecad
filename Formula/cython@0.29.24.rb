efa7e0d77b9bdb2122d4a73f801ec0e3b3ded268
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-10-20 22:01:56 +0200

    Fix missing setuptools

--- Formula/cython@0.29.24.rb
+++ Formula/cython@0.29.24.rb
@@ -12,16 +12,17 @@ class CythonAT02924 < Formula
   depends_on "./python@3.9.7"
 
   def install
-    xy = Language::Python.major_minor_version Formula["./python@3.9.7"].opt_bin/"python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system Formula["./python@3.9.7"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+    py = Formula["./python@3.9.7"]
+    xy = Language::Python.major_minor_version py.opt_bin/"python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages:#{py.site_packages}"
+    system py.opt_bin/"python3", *Language::Python.setup_install_args(libexec)
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
-    xy = Language::Python.major_minor_version Formula["python@3.9./"].opt_bin/"python3"
+    xy = Language::Python.major_minor_version Formula["./python@3.9.7"].opt_bin/"python3"
     ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
 
     phrase = "You are using Homebrew"
