c7fe10e6eddaa0b589ee9406696e3d68a5bf2a8f
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-11-07 18:34:11 +0100

    Switch to opencascade@7.5.1

--- Formula/pythonocc-core.rb
+++ Formula/pythonocc-core.rb
@@ -4,13 +4,13 @@ class PythonoccCore < Formula
   url "https://github.com/tpaviot/pythonocc-core.git", :using => :git
   version "7.5.0"
 
-  depends_on "./opencascade@7.5.3" => :required
+  depends_on "./opencascade@7.5.1" => :required
   depends_on "cmake" => :build
   depends_on "./python@3.9.7"
 
   def install
     mkdir "Build" do
-     system "cmake", '-DOCE_LIB_PATH=' + Formula["./opencascade@7.5.3"].lib, '-DOCE_INCLUDE_PATH=' + Formula["#@tap/opencascade@7.5.3"].include + '/opencascade', *std_cmake_args , ".."
+     system "cmake", '-DOCE_LIB_PATH=' + Formula["./opencascade@7.5.1"].lib, '-DOCE_INCLUDE_PATH=' + Formula["#@tap/opencascade@7.5.1"].include + '/opencascade', *std_cmake_args , ".."
      system "make", "-j#{ENV.make_jobs}"
      system "make", "install"
     end
