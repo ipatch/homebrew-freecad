81da7bd16c5514ba99e91c539dc5e484e4cce64a
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-11-28 10:18:31 +0100

    Revert "Update to 7.5.3"
    This reverts commit c0085e3bd1abca9a137146db622235fb4704677f.

--- Formula/pythonocc-core.rb
+++ Formula/pythonocc-core.rb
@@ -2,9 +2,9 @@ class PythonoccCore < Formula
   desc "Pythonocc provides 3D modeling and dataexchange features. It is intended to CAD/PDM/PLM and BIM related development."
   homepage "https://www.pythonocc.org"
   url "https://github.com/tpaviot/pythonocc-core.git", :using => :git
-  version "7.5.3"
+  version "7.5.2"
 
-  depends_on "./opencascade@7.5.3" => :required
+  depends_on "./opencascade@7.5.2" => :required
   depends_on "cmake" => :build
   depends_on "./python@3.9.7"
 
@@ -21,7 +21,7 @@ class PythonoccCore < Formula
     end    
     
     mkdir "Build" do
-     system "cmake", '-DOCE_LIB_PATH=' + Formula["./opencascade@7.5.3"].lib, '-DOCE_INCLUDE_PATH=' + Formula["./opencascade@7.5.3"].include + '/opencascade', *std_cmake_args , ".."
+     system "cmake", '-DOCE_LIB_PATH=' + Formula["./opencascade@7.5.2"].lib, '-DOCE_INCLUDE_PATH=' + Formula["./opencascade@7.5.2"].include + '/opencascade', *std_cmake_args , ".."
      system "make", "-j#{ENV.make_jobs}"
      system "make", "install"
     end
