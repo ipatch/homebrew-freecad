31888fddc6c5dbfff321a178848c85fb184a7e5a
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2022-04-23 20:05:37 +0200

    Update llvm@13.0.0.rb

--- Formula/llvm@13.0.0.rb
+++ Formula/llvm@13.0.0.rb
@@ -33,7 +33,7 @@ class LlvmAT1300 < Formula
     # See: Homebrew/homebrew-core/issues/35513
     depends_on "cmake" => :build
     depends_on "./swig@4.0.2" => :build
-    depends_on "./python@3.9.7"
+    depends_on "./python@3.10.2"
 
     uses_from_macos "libedit"
     uses_from_macos "libffi", since: :catalina
