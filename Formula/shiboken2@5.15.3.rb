4d473ea378b465a2a17296f85393f08e5a823e93
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2022-04-26 20:25:04 +0200

    Create shiboken2@5.15.3.rb

---
+++ Formula/shiboken2@5.15.3.rb
@@ -0,0 +1,58 @@
+class Shiboken2AT5152 < Formula
+  desc "GeneratorRunner plugin that outputs C++ code for CPython extensions"
+  homepage "https://code.qt.io/cgit/pyside/pyside-setup.git/tree/README.shiboken2-generator.md?h=5.15.2"
+  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.3-src/pyside-setup-opensource-src-5.15.3.zip"
+  sha256 "ae8517173eb831301791dd864ba73e8c51b9fab67eeb8d2ed76ddfe20377ab03"
+
+  #bottle do
+  #  root_url "https://github.com/FreeCAD/homebrew-freecad/releases/download/shiboken2@5.15.2-5.15.2"
+  #  sha256 cellar: :any, big_sur:  "b8d2ad961130d7e8f6a838bc95b55c12d529befe6a0cd03aebef7792abd060e6"
+  #  sha256 cellar: :any, catalina: "43d87877ce4168d1d6c5574cf4dc26683845ed8fa74d5ca23aa9174dc167db8d"
+  #  sha256 cellar: :any, mojave:   "3ee35e362c5b373328c53f9a85927b1282e32ccf542343de359c3d96405ceef3"
+  #end
+
+  keg_only :versioned_formula # NOTE: will conflict with other shiboken2 installs
+
+  depends_on "cmake" => :build
+  depends_on "./python@3.10.2" => :build
+  depends_on "./numpy@1.22.3"
+  depends_on "./qt5153"
+  depends_on "./llvm@13.0.0"
+
+  patch do
+    url "https://src.fedoraproject.org/rpms/python-pyside2/raw/1f7e56d4f8320d98e59d9291ddab0451a436c7bf/f/python3.10.patch"
+    sha256 "dd18a7ed1d4196b48ddfd7cf1dcbebe4c600dcedb45baba4c852131e221cf4bc"
+  end
+
+  def install
+    ENV["LLVM_INSTALL_DIR"] = Formula["./llvm@13.0.0"].opt_prefix
+
+    mkdir "macbuild#{version}" do
+      pyhome = `#{Formula["./python@3.10.2"].opt_bin}/python3.10-config --prefix`.chomp
+      py_library = "#{pyhome}/lib/libpython3.10.dylib"
+      py_include = "#{pyhome}/include/python3.10"
+      args = std_cmake_args
+      # Building the tests, is effectively a test of Shiboken
+      args << "-DBUILD_TYPE=Release"
+      args << "-DBUILD_TESTS:BOOL=OFF"
+      args << "-DPYTHON_EXECUTABLE=#{pyhome}/bin/python3.10"
+      args << "-DPYTHON_LIBRARY=#{py_library}"
+      args << "-DPYTHON_INCLUDE_DIR=#{py_include}"
+      args << "../sources/shiboken2"
+
+      system "cmake", *args
+      system "make", "-j#{ENV.make_jobs}", "install"
+    end
+  end
+
+  def caveats
+    <<-EOS
+    this formula is keg-only due to freecad/freecad/shiboken2
+    EOS
+  end
+
+  test do
+    # NOTE: use `#{bin}` able to test formula installed in custom prefix
+    system "#{bin}/shiboken2", "--version"
+  end
+end
