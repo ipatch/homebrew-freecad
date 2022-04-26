7586435bfd950345bf37f73206652cf801175a6d
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2022-04-26 09:46:44 +0200

    Update shiboken2@5.15.2.rb

--- Formula/shiboken2@5.15.2.rb
+++ Formula/shiboken2@5.15.2.rb
@@ -1,8 +1,9 @@
 class Shiboken2AT5152 < Formula
   desc "GeneratorRunner plugin that outputs C++ code for CPython extensions"
   homepage "https://code.qt.io/cgit/pyside/pyside-setup.git/tree/README.shiboken2-generator.md?h=5.15.2"
-  url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.2-src/pyside-setup-opensource-src-5.15.2.tar.xz"
-  sha256 "b306504b0b8037079a8eab772ee774b9e877a2d84bab2dbefbe4fa6f83941418"
+  url "http://code.qt.io/cgit/pyside/pyside-setup.git",
+    tag:      "v5.15.2.1",
+    revision: "9282e03de471bb3772c8d3997159e49c113d7678"
 
   #bottle do
   #  root_url "https://github.com/FreeCAD/homebrew-freecad/releases/download/shiboken2@5.15.2-5.15.2"
@@ -19,10 +20,10 @@ class Shiboken2AT5152 < Formula
   depends_on "./qt5152"
   depends_on "./llvm@13.0.0"
 
-  patch do
-    url "https://src.fedoraproject.org/rpms/python-pyside2/raw/1f7e56d4f8320d98e59d9291ddab0451a436c7bf/f/python3.10.patch"
-    sha256 "dd18a7ed1d4196b48ddfd7cf1dcbebe4c600dcedb45baba4c852131e221cf4bc"
-  end
+  #patch do
+  #  url "https://src.fedoraproject.org/rpms/python-pyside2/raw/1f7e56d4f8320d98e59d9291ddab0451a436c7bf/f/python3.10.patch"
+  #  sha256 "dd18a7ed1d4196b48ddfd7cf1dcbebe4c600dcedb45baba4c852131e221cf4bc"
+  #end
 
   def install
     ENV["LLVM_INSTALL_DIR"] = Formula["./llvm@13.0.0"].opt_prefix
